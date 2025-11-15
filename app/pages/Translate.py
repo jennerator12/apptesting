6fe3a72c8993c11472b7dd5352b18235cb4c3659
jennerator12 <wabisabilithium12@gmail.com>
2025-11-15 19:55:57 +0000

    changed name

--- app/pages/Translate.py
+++ app/pages/Translate.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from transformers import pipeline
 
-st.title("Humor Adapter (No OpenAI)")
+st.title("Humor Adapter")
 
 options = ["Idiom", "Emoji", "Joke"] 
 selection = st.segmented_control( "Type of Humor", options, selection_mode="single" ) 
